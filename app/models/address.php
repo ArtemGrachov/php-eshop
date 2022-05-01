10df761031e01f9e81e80bf0d5351bb0743b0437
Artem Grachov <grachov2195@gmail.com>
2022-05-01 23:18:29 +0300

    Customer and address models

---
+++ app/models/address.php
@@ -0,0 +1,113 @@
+<?php
+require_once(__DIR__ . '/../models/database.php');
+
+class ModelAddress {
+    public $id = null;
+    public $country = null;
+    public $region = null;
+    public $city = null;
+    public $street = null;
+    public $houseNumber = null;
+    public $appartmentNumber = null;
+    public $notes = null;
+
+    public static function getAddresses() {
+        $db = Database::getInstance()->db;
+
+        $query = 'SELECT A.country, A.region, A.city, A.street, A.houseNumber, A.appartmentNumber,
+                  A.notes FROM addresses A ORDER BY A.id';
+
+        $statement = $db->prepare($query);
+        $statement->execute();
+
+        $rawAddresses = $statement->fetchAll();
+
+        $statement->closeCursor();
+
+        $addresses = array_map(function($rawAddress) {
+            return new ModelAddress($rawAddress);
+        }, $rawAddresses);
+
+        return $addresses;
+    }
+
+    public static function getAddress($addressId) {
+        $db = Database::getInstance()->db;
+
+        $query = 'SELECT * FROM addresses WHERE id = :addressId';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':addressId', $addressId);
+        $statement->execute();
+
+        $address = $statement->fetch();
+
+        $statement->closeCursor();
+
+        return new ModelAddress($address);
+    }
+
+    public function __construct($payload) {
+        $this->id = isset($payload['id']) ? $payload['id'] : null;
+        $this->country = isset($payload['country']) ? $payload['country'] : null;
+        $this->region = isset($payload['region']) ? $payload['region'] : null;
+        $this->city = isset($payload['city']) ? $payload['city'] : null;
+        $this->street = isset($payload['street']) ? $payload['street'] : null;
+        $this->houseNumber = isset($payload['houseNumber']) ? $payload['houseNumber'] : null;
+        $this->appartmentNumber = isset($payload['appartmentNumber']) ? $payload['appartmentNumber'] : null;
+        $this->notes = isset($payload['notes']) ? $payload['notes'] : null;
+    }
+
+    public function save() {
+        $db = Database::getInstance()->db;
+
+        if (is_null($this->id)) {
+            $query = 'INSERT INTO addresses (country, region, city, street, houseNumber, appartmentNumber, notes)
+                      VALUES (:country, :region, :city, :street, :houseNumber, :appartmentNumber, :notes)';
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':country', $this->country);
+            $statement->bindValue(':region', $this->region);
+            $statement->bindValue(':city', $this->city);
+            $statement->bindValue(':street', $this->street);
+            $statement->bindValue(':houseNumber', $this->houseNumber);
+            $statement->bindValue(':appartmentNumber', $this->appartmentNumber);
+            $statement->bindValue(':notes', $this->notes);
+
+            $statement->execute();
+
+            $statement->closeCursor();
+        } else {
+            $query = 'UPDATE addresses SET country = :country, region = :region, city = :city,
+                      street = :street, houseNumber = :houseNumber,
+                      appartmentNumber = :appartmentNumber, notes = :notes
+                      WHERE id=:addressId';
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':country', $this->country);
+            $statement->bindValue(':region', $this->region);
+            $statement->bindValue(':city', $this->city);
+            $statement->bindValue(':street', $this->street);
+            $statement->bindValue(':houseNumber', $this->houseNumber);
+            $statement->bindValue(':appartmentNumber', $this->appartmentNumber);
+            $statement->bindValue(':notes', $this->notes);
+            $statement->execute();
+
+            $statement->closeCursor();
+        }
+    }
+
+    public function remove() {
+        $db = Database::getInstance()->db;
+
+        $query = 'DELETE FROM addresses WHERE ID = :addressId';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':addressId', $this->id);
+        $statement->execute();
+
+        $statement->closeCursor();
+
+        $this->id = null;
+    }
+}
---
+++ app/models/customer.php
@@ -0,0 +1,113 @@
+<?php
+require_once(__DIR__ . '/../models/database.php');
+
+class ModelCustomer {
+    public $id = null;
+    public $firstName = null;
+    public $lastName = null;
+    public $email = null;
+    public $phoneNumber = null;
+    public $isCompany = false;
+    public $companyName = null;
+    public $companyVatNumber = null;
+
+    public static function getCustomers() {
+        $db = Database::getInstance()->db;
+
+        $query = 'SELECT C.id, C.firstName, C.firstName, C.email, C.phoneNumber, C.isCompany,
+                  C.companyName, C.companyVatNumber FROM customers C ORDER BY C.id';
+
+        $statement = $db->prepare($query);
+        $statement->execute();
+
+        $rawCustomers = $statement->fetchAll();
+
+        $statement->closeCursor();
+
+        $customers = array_map(function($rawCustomer) {
+            return new ModelCustomer($rawCustomer);
+        }, $rawCustomers);
+
+        return $customers;
+    }
+
+    public static function getCustomer($customerId) {
+        $db = Database::getInstance()->db;
+
+        $query = 'SELECT * FROM customers WHERE id = :customerId';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':customerId', $customerId);
+        $statement->execute();
+
+        $customer = $statement->fetch();
+
+        $statement->closeCursor();
+
+        return new ModelCustomer($customer);
+    }
+
+    public function __construct($payload) {
+        $this->id = isset($payload['id']) ? $payload['id'] : null;
+        $this->firstName = isset($payload['firstName']) ? $payload['firstName'] : null;
+        $this->lastName = isset($payload['lastName']) ? $payload['lastName'] : null;
+        $this->email = isset($payload['email']) ? $payload['email'] : null;
+        $this->phoneNumber = isset($payload['phoneNumber']) ? $payload['phoneNumber'] : null;
+        $this->isCompany = isset($payload['isCompany']) ? $payload['isCompany'] : null;
+        $this->companyName = isset($payload['companyName']) ? $payload['companyName'] : null;
+        $this->companyVatNumber = isset($payload['companyVatNumber']) ? $payload['companyVatNumber'] : null;
+    }
+
+    public function save() {
+        $db = Database::getInstance()->db;
+
+        if (is_null($this->id)) {
+            $query = 'INSERT INTO customers (firstName, lastName, email, phoneNumber, isCompany, companyName, companyVatNumber)
+                      VALUES (:firstName, :lastName, :email, :phoneNumber, :isCompany, :companyName, :companyVatNumber)';
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':firstName', $this->firstName);
+            $statement->bindValue(':lastName', $this->lastName);
+            $statement->bindValue(':email', $this->email);
+            $statement->bindValue(':phoneNumber', $this->phoneNumber);
+            $statement->bindValue(':isCompany', $this->isCompany);
+            $statement->bindValue(':companyName', $this->companyName);
+            $statement->bindValue(':companyVatNumber', $this->companyVatNumber);
+            $statement->execute();
+
+            $statement->closeCursor();
+        } else {
+            $query = 'UPDATE customers SET name = firstName = :firstName, lastName = :lastName,
+                      email = :email, phoneNumber = :phoneNumber, isCompany = :isCompany,
+                      companyName = :companyName, companyVatNumber = :companyVatNumber
+                      WHERE id=:customerId';
+
+            $statement = $db->prepare($query);
+            $statement->bindValue(':customerId', $this->id);
+            $statement->bindValue(':firstName', $this->firstName);
+            $statement->bindValue(':lastName', $this->lastName);
+            $statement->bindValue(':email', $this->email);
+            $statement->bindValue(':phoneNumber', $this->phoneNumber);
+            $statement->bindValue(':isCompany', $this->isCompany);
+            $statement->bindValue(':companyName', $this->companyName);
+            $statement->bindValue(':companyVatNumber', $this->companyVatNumber);
+            $statement->execute();
+
+            $statement->closeCursor();
+        }
+    }
+
+    public function remove() {
+        $db = Database::getInstance()->db;
+
+        $query = 'DELETE FROM customers WHERE ID = :customerId';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':customerId', $this->id);
+        $statement->execute();
+
+        $statement->closeCursor();
+
+        $this->id = null;
+    }
+}
